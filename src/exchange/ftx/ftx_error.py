6b8128feb27af75c8f851f6cbddc7a197331cbc0
howardatsteaker <88700876+howardatsteaker@users.noreply.github.com>
2022-08-24 14:47:58 +0800

    fix/ftx errors

--- src/exchange/ftx/ftx_error.py
+++ src/exchange/ftx/ftx_error.py
@@ -6,3 +6,31 @@ class ExchangeError(Exception):
 
     def __init__(self, *args: object) -> None:
         super().__init__(*args)
+
+
+class RateLimitExceeded(ExchangeError):
+    pass
+
+
+class InsufficientFunds(ExchangeError):
+    pass
+
+
+class InvalidOrder(ExchangeError):
+    pass
+
+
+ftx_error_message_map = {
+    'Not enough balances': InsufficientFunds,
+    'Account does not have enough margin for order': InsufficientFunds,
+    'Please slow down': RateLimitExceeded,
+    'Do not send more than': RateLimitExceeded,
+    'Invalid reduce-only order': InvalidOrder,
+}
+
+
+def ftx_throw_exception(message: str) -> ExchangeError:
+    for key in ftx_error_message_map:
+        if message in key:
+            raise ftx_error_message_map[key](message)
+    raise ExchangeError(message)
